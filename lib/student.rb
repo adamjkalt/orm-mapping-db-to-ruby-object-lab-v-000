88d609dcf2f3a9e732401a1ad487946ee05f6e8b
Adam Kalt <adam.kalt.1@gmail.com>
2018-06-03 20:09:25 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,9 +58,19 @@ class Student
   end
 
 def self.count_all_students_in_grade_9
+  sql = <<-SQL
+     SELECT *
+     FROM students
+     WHERE grade == 9
+   SQL
 end
 
 def self.students_below_12th_grade
+  sql = <<-SQL
+     SELECT *
+     FROM students
+     WHERE grade < 12
+   SQL
 end
 
 def self.first_X_students_in_grade_10
