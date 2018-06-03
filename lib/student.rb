3ba36df7e751bae8ada78f0b0e4304e5fc90a0ae
Adam Kalt <adam.kalt.1@gmail.com>
2018-06-03 20:13:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -87,7 +87,7 @@ def self.first_X_students_in_grade_10
     ORDER BY students.id
     LIMIT ?
   SQL
-  DB[:conn].execute(sql).map do |row|
+  DB[:conn].execute(sql, number).map do |row|
     self.new_from_db(row)
   end
 end
