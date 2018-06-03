0917c450de363395e0dba4f8eb4f27fb729579be
Adam Kalt <adam.kalt.1@gmail.com>
2018-06-03 20:11:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -80,6 +80,15 @@ def self.students_below_12th_grade
 end
 
 def self.first_X_students_in_grade_10
+  sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade == 10
+    LIMIT ?
+  SQL
+  DB[:conn].execute(sql).map do |row|
+    self.new_from_db(row)
+  end
 end
 
 def self.first_student_in_grade_10
