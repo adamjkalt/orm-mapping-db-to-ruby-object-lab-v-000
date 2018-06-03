b47b19bb2e7a37265c9bddf00a490cc55dc51d5e
Adam Kalt <adam.kalt.1@gmail.com>
2018-06-03 20:20:36 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -106,6 +106,15 @@ def self.first_student_in_grade_10
 end
 
 def self.all_students_in_grade_X
+  sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = X
+    ORDER BY students.id
+  SQL
+  DB[:conn].execute(sql).map do |row|
+    self.new_from_db(row)
+  end
 end
 
 end
